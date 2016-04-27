ddff74b7b8b5505418450109bb794595bddb9861
Dominik Pielak <dominikpielak@gmail.com>
2016-04-27 20:34:32 +0200

    Warsztaty II
    mini gra pozwalająca zdobyć pierwsze ogniwo energetyczne do otwarcia
    drzwi

---
+++ Assets/Scripts/GeneratorPowerState.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GeneratorPowerState : MonoBehaviour
+{
+    public Player Player;
+    public Image PowerStateImage;
+
+    public Sprite[] States;
+
+    private int currentCellsDisplayed;
+
+    private void Start()
+    {
+        currentCellsDisplayed = Player.CollectedCells;
+        SetCorrectImage();
+    }
+
+    private void Update()
+    {
+        if(Player.CollectedCells != currentCellsDisplayed)
+        {
+            currentCellsDisplayed = Player.CollectedCells;
+            SetCorrectImage();
+        }
+    }
+
+    private void SetCorrectImage()
+    {
+        PowerStateImage.sprite = States[currentCellsDisplayed];
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,25 +5,34 @@ using System;
 public class Player : MonoBehaviour
 {
     public Transform PlayerTransform;
+    public Transform CocountPosition;
     public CharacterController PlayerChController;
     public Camera PlayerCamera;
 
     public float Speed = 5;
+    public int CollectedCells { get; private set; }
+    public bool HaveCocount { get { return currentCocount != null; } }
     [SerializeField]
     private Vector2 RotationSensivity;
     private Vector3 prevMousePosRel;
-    // Use this for initialization
-    void Start ()
+    private Cocount currentCocount;
+
+    private void Start ()
     {
         prevMousePosRel = new Vector3(0.5f, 0.5f);
+        CollectedCells = 0;
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+
+    private void Update ()
     {
         Move();
         RotatePlayer();
-	}
+        if (HaveCocount)
+        {
+            ThrowCocount();
+        }
+    }
 
     private void RotatePlayer()
     {
@@ -44,5 +53,34 @@ public class Player : MonoBehaviour
             PlayerTransform.Rotate(Vector3.up, input.x);
             PlayerChController.SimpleMove(PlayerTransform.forward * input.y* Speed);
         }
+    }
+
+    private void ThrowCocount()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && !currentCocount.HasThrown)
+        {
+            currentCocount.Throw(PlayerCamera.ScreenPointToRay(Input.mousePosition));
+        }
+    }
+
+    public void AddCocount(Cocount cocount)
+    {
+        currentCocount = cocount;
+        cocount.transform.SetParent(CocountPosition);
+        cocount.transform.localPosition = Vector3.zero;
+    }
+
+    public void RemoveCocount()
+    {
+        if(currentCocount != null)
+        {
+            Destroy(currentCocount.gameObject);
+        }
+        currentCocount = null;
+    }
+
+    public void CollectCell()
+    {
+        CollectedCells++;
     }
 }
